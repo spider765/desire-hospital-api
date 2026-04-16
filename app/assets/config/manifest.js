e4a01ef8110e9505ecb1f10e55084ebb5d359fc7
spider765 <danejukwo11@gmail.com>
2026-04-16 01:18:00 +0100

    Remove production credentials, use master key only

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,4 @@
 // app/assets/config/manifest.js
 
-//= link_tree ../images
-//= link_directory ../javascripts .js
-//= link_directory ../stylesheets .css
+//= link rails_admin/application.css
+//= link rails_admin/application.js
---
+++ public/assets/manifest-1109bf878f733cf749671fe3db9090ee7379d4b1c6b498c156715952449bb28d.js
@@ -0,0 +1,5 @@
+// app/assets/config/manifest.js
+
+
+
+;
