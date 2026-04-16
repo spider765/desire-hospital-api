51801c0f550c42793678ba21ca8d6a80507fb2e0
spider765 <danejukwo11@gmail.com>
2026-04-16 02:55:23 +0100

    Remove production credentials, use master key only

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,12 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # Added a comma between the two URLs and added credentials: true
-    origins 'http://localhost:3000', 'https://desire-hospital-ui-c5mn.vercel.app'
-    
+    # You must have a COMMA between the two URLs
+    origins 'http://localhost:3000', 
+            'https://desire-hospital-ui-c5mn.vercel.app',
+            'https://desire-hospital-ui-c5mn-o7qhw1fqm-spider765s-projects.vercel.app'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+      credentials: true # This must be true for your admin login to work
   end
 end
