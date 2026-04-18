f27b781f5a5e45cbf97bc538b44fb313c9195ea9
spider765 <danejukwo11@gmail.com>
2026-04-18 12:43:47 +0100

    Fix AppointmentsController missing class definition

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,12 +1,13 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # You must have a COMMA between the two URLs
-    origins 'http://localhost:3000', 
-            'https://desire-hospital-ui-c5mn.vercel.app',
-            'https://desire-hospital-ui-c5mn-o7qhw1fqm-spider765s-projects.vercel.app'
-    resource '*',
+    origins "http://localhost:3000",
+            "https://desire-hospital-ui-c5mn.vercel.app",
+            "https://desire-hospital-ui-c5mn-o7qhw1fqm-spider765s-projects.vercel.app",
+            "https://desire-hospital-ui-c5mn-ooxqdnac2-spider765s-projects.vercel.app"
+    resource "*",
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
       credentials: true # This must be true for your admin login to work
   end
 end
